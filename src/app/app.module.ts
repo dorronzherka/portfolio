bc444c14544641cf4bebd21ee000a26209f12bfb
Dorron Zherka <dorron.zherka@gmail.com>
2018-09-17 00:21:04 +0200

    Header, main, footer, component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes, Route } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/main/header/header.component';
+import { FooterComponent } from './layouts/main/footer/footer.component';
+import { LayoutComponent } from './layouts/main/layout/layout.component';
+import { HomeComponent } from './main/home/home.component';
 
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {path: '', component: HomeComponent}
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
